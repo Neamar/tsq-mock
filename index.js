421d861d41af804b8e38fc829d832b4330d0969d
Neamar <neamar@neamar.fr>
2018-06-28 14:58:18 +0100

    Updated mock

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 var gcm = new GCM(process.env.GCM_KEY);
 
-app.post('/question/:id', (req, res) => {
+app.post('/questions/:id', (req, res) => {
   console.log('Posted answer');
   res.send('POSTED answer');
 });
@@ -49,7 +49,7 @@ app.get('/fake/new_question', function(req, res) {
     'data.type': 'new_question',
     'data.timeout': 5,
     'data.question_id': 1,
-    'data.question': 'Test',
+    'data.question': 'Alberto or foo or bar?',
     'data.answers': "['Foo', 'Bar']"
   };
 
@@ -64,6 +64,29 @@ app.get('/fake/new_question', function(req, res) {
   });
 });
 
+
+app.get('/fake/new_answer', function(req, res) {
+  // {question: "Test", answers: ["Foo", "Bar"], timeout: 5, id: 1}
+  const message = {
+    registration_id: 'eBjNyz5Pg90:APA91bEi2IPqklgAJchcB1Er-tv-GjXQrtwRPIOQy4V_SkfDR6jhWlI95DjWbNR1A4IRbCWr2B3dtmNQ0U89hf96_JKbEVobr7cDYxjwYw_xhrQ-Gy9REhHgwJQVzpYYaMwI09O9_V-MeZRod29YrRqO_lLpeGyeSw',
+    time_to_live: 0,
+    'data.type': 'new_question',
+    'data.timeout': 5,
+    'data.question_id': 1,
+    'data.question': 'Alberto or foo or bar?',
+    'data.answers': `[{"answer":"Foo", "percentage": 25}, {"answer":"Bar", "percentage": 25}]`
+  };
+
+  gcm.send(message, function(err, messageId) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log('Sent with message ID: ', messageId);
+      res.send(messageId);
+    }
+  });
+});
 
 module.exports.signalLeaderboardUpdate = function() {
   // {id: 123}
